2c5a59e6a63a35ddb78f58e889f577d68c3cf941
Yifan Liu <39271063+kokonoseren@users.noreply.github.com>
2018-12-15 01:09:30 +0800

    Update hashtrietree.h

--- hashtrietree.h
+++ hashtrietree.h
@@ -12,9 +12,11 @@ class HashTrieTree
 public:
     HashTrieTree();//构造一个空树
     ~HashTrieTree();//销毁一个数
-    void word_to_chs(const string & word, vector<string> & chs);//把中文词语分成一个一个字
-    void search_node(const string & word, int &weight, string &kind);//查找一个单词是否存在
-    void insert_node(const string & word, const int &weight, const string &kind);//插入节点
+    static void word_to_chs_UTF8(const string & word, vector<string> & chs);//把中文词语分成一个一个字
+    static void word_to_chs_GBK(const string & word, vector<string> & chs);//把中文词语分成一个一个字
+    void search_node(const string & word, double &weight, string &kind);//查找一个单词是否存在
+    void insert_node(const string & word, const double &weight, const string &kind);//插入节点
+
 private:
     TrieNode *_root;//根节点的指针
 };
