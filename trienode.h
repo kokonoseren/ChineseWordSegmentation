a6b198cf0c52d5d98aa4b13caa49550370864f2f
Yifan Liu <39271063+kokonoseren@users.noreply.github.com>
2018-12-15 01:11:03 +0800

    Update trienode.h

--- trienode.h
+++ trienode.h
@@ -20,7 +20,7 @@ public:
     ~TrieNode(){}//销毁节点todo
 
     int _num;              //经过该节点的词数；如果是根节点，则填总词数
-    int _isWord;          //标记该结点处是否构成单词,如果构成，那么写成词频
+    double _isWord;          //标记该结点处是否构成单词,如果构成，那么写成词频
     string _kind;          //如果构成单词，标记该节点的词性
     std::unordered_map < string, TrieNode* > _words;
     //存储若干个子节点，可以通过下一个自直接查找到下一个节点
@@ -32,3 +32,4 @@ typedef unordered_map<string,TrieNode*>::iterator it_word;
 //这个迭代器用来遍历当前节点的所有子节点
 
 #endif // TRIENODE_H
+
