f5a1598c6615dbc178f4fbc5235cbe3b6d745937
Yifan Liu <39271063+kokonoseren@users.noreply.github.com>
2018-12-22 11:11:32 +0800

    Update sentencegraph.cpp

--- sentencegraph.cpp
+++ sentencegraph.cpp
@@ -61,17 +61,17 @@ SentenceGraph::SentenceGraph(const string &Sentence,HashTrieTree *T){
         node->pos=i;//初始化顶点节点的字，不更改它的弧信息,pos=1表示这个节点在第一个字之前
         ArcNode *newNode1=new ArcNode;
         newNode1->_adj_vex=i+1;
-        newNode1->_weight=0;
+        newNode1->_weight=INT_MAX;
         newNode1->_next_arc=NULL;
         node->first_arc=newNode1;
         //接下来寻找它的弧
         for(unsigned int j=i;j<chs.size();j++){//对于chs里的每一个字，寻找它能和后续的字构成的可能词语
             possible_word+=chs[j];
-            double weight=0;
+            double weight=INT_MAX;
             string kind;
             T->search_node(possible_word,weight,kind);
 
-            if(weight && j==i){//如果构成词语，则更改它的弧信息
+            if(weight<INT_MAX && j==i){//如果构成词语，则更改它的弧信息
                 ArcNode *newNode=new ArcNode;
                 newNode->_kind=kind;
                 newNode->_adj_vex=j+1;
@@ -81,7 +81,7 @@ SentenceGraph::SentenceGraph(const string &Sentence,HashTrieTree *T){
                 node->first_arc=newNode;
                 this->_arc_num++;
             }
-            else if(weight && j!=i){
+            else if(weight<INT_MAX && j!=i){
                 ArcNode *temp=node->first_arc;
                 while(temp->_next_arc!=NULL){
                     temp=temp->_next_arc;
@@ -106,7 +106,7 @@ SentenceGraph::SentenceGraph(const string &Sentence,HashTrieTree *T){
     }
     VexNode *node=new VexNode;//要插入的顶点节点
 
-    node->pos=chs.size()+1;//初始化顶点节点的字，不更改它的弧信息,pos=1表示这个节点在第一个字之前
+    node->pos=chs.size()+1;//初始化顶点节点的字，不更改它的弧信息,pos=0表示这个节点在第一个字之前
     node->first_arc=NULL;
     this->_vex.push_back(*node);
     delete node;
@@ -114,11 +114,15 @@ SentenceGraph::SentenceGraph(const string &Sentence,HashTrieTree *T){
 }
 
 void SentenceGraph::shortest_cut(stack<int> &path){
-    //todo如何保存路径？直接实现n-最短路径？
+    //todo直接实现n-最短路径？
     queue<int> vex_nodes;
     int pre_record[this->_vex_num]={0};
-    int dist[this->_vex_num]={0};//最大概率路径数组
-    //bool path[this->_vex_num][this->_vex_num];
+    int dist[this->_vex_num];//最大概率路径数组
+    dist[0]=0;
+    for(int i=1;i<this->_vex_num;i++){
+        dist[i]=INT_MAX;
+    }
+    //第0个节点_vex[0]
     ArcNode *p=new ArcNode;
     p=this->_vex[0].first_arc;
     while(p){
@@ -127,6 +131,8 @@ void SentenceGraph::shortest_cut(stack<int> &path){
         vex_nodes.push(p->_adj_vex);
         p=p->_next_arc;
     }
+
+    //处理后面的节点
     while(!vex_nodes.empty()){
         int pre_vex=vex_nodes.front();
         vex_nodes.pop();
@@ -134,7 +140,7 @@ void SentenceGraph::shortest_cut(stack<int> &path){
             continue;
         p=this->_vex[pre_vex].first_arc;
         while(p){
-            if(dist[p->_adj_vex] < (dist[pre_vex] + p->_weight)){
+            if(dist[p->_adj_vex] > (dist[pre_vex] + p->_weight)){
                 dist[p->_adj_vex] = dist[pre_vex] + p->_weight;
                 pre_record[p->_adj_vex]=pre_vex;
                 vex_nodes.push(p->_adj_vex);
@@ -142,6 +148,52 @@ void SentenceGraph::shortest_cut(stack<int> &path){
             p=p->_next_arc;
         }
     }
+
+    int k=1;
+    int m=this->_vex_num-1;
+    while(k){
+        k=pre_record[m];
+        if(k)path.push(k);
+        m=k;
+    }
+}
+
+void SentenceGraph::shortest_cut(stack<int> &path){
+    //todo直接实现n-最短路径？
+    queue<int> vex_nodes;
+    int pre_record[this->_vex_num]={0};
+    int dist[this->_vex_num];//最大概率路径数组
+    dist[0]=0;
+    for(int i=1;i<this->_vex_num;i++){
+        dist[i]=INT_MAX;
+    }
+    //第0个节点_vex[0]
+    ArcNode *p=new ArcNode;
+    p=this->_vex[0].first_arc;
+    while(p){
+        dist[p->_adj_vex]=p->_weight;
+        //dist[p->_adj_vex]=1;
+        vex_nodes.push(p->_adj_vex);
+        p=p->_next_arc;
+    }
+
+    //处理后面的节点
+    while(!vex_nodes.empty()){
+        int pre_vex=vex_nodes.front();
+        vex_nodes.pop();
+        if(pre_vex==this->_vex_num-1)//最后一个节点的first_arc是
+            continue;
+        p=this->_vex[pre_vex].first_arc;
+        while(p){
+            if(dist[p->_adj_vex] > (dist[pre_vex] + p->_weight)){
+                dist[p->_adj_vex] = dist[pre_vex] + p->_weight;
+                pre_record[p->_adj_vex]=pre_vex;
+                vex_nodes.push(p->_adj_vex);
+            }
+            p=p->_next_arc;
+        }
+    }
+
     int k=1;
     int m=this->_vex_num-1;
     while(k){
